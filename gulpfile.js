dc00972e901f494b4270c0008d9e8b55d34d416d
richard chen <clq_web@126.com>
2015-03-04 15:51:10 +0800

    v0.2.1

--- gulpfile.js
+++ gulpfile.js
@@ -17,14 +17,17 @@ require('./lib/task.js')(gulp, cfg);
 gulp.task('default', function() {
     var str = gutil.colors.green('\r\n' +
         '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\r\n' +
-        '              欢迎使用 gulp-simple-vm\r\n' +
+        '              Welcome to gulp-simple-vm\r\n' +
         '--------------------------------------------------------------\r\n\r\n' +
-        'gulp build: 生成命令\r\n' +
-        '   -[o|optimize]: optimize css, js and image files\r\n\r\n' +
-        'gulp server: 启动http服务器\r\n' +
-        '   -[o|optimize]: optimize css, js and image files\r\n' +
-        'gulp deploy: 复制VM文件和"build"目录为"vyyyyMMdd[version]"格式\r\n' +
-        '   -v[version]: 当天布署的次数作为版本号 \r\n\r\n' +
+        'Usage: gulp <command>\r\n\r\n' +
+        'Comamnds:\r\n' +
+        '  build: build project to "build" directory\r\n' +
+        '    -[o|optimize]: optimize css, js and image files\r\n\r\n' +
+        '  server: open http server, "build" as root，so use "gulp build" first\r\n' +
+        '    -[o|optimize]: optimize css, js and image files\r\n' +
+        '    -[b|browser]: auto open browser\r\n\r\n' +
+        '  deploy: copy "build" directory as "vyyyyMMdd[version]"\r\n' +
+        '    -v[version]: version number \r\n\r\n' +
         '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\r\n');
 
     console.log(str);
